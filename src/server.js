2b4c948455aee0c35b32883c84a6bdf4377509f6
hoangvan1410 <76679911+hoangvan1410@users.noreply.github.com>
2021-06-03 14:24:14 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,8 +5,8 @@ const options = require('./admin.options');
 const buildAdminRouter = require('./admin.router');
 
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT || 3000
+var host = process.env.HOST || '0.0.0.0'
 const run = async () => {
   await mongoose.connect('mongodb+srv://admin:1234@cluster0.ewayb.mongodb.net/databases?retryWrites=true&w=majority', {
     useNewUrlParser: true,
